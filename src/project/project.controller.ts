b179f9b9b58c1a840e9110b757d288c40a0b1fe0
DmitryMass <dimamikron41@gmail.com>
2023-11-29 17:18:42 +0200

    added deleting project route with deleting relations and files from cdn

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -4,6 +4,8 @@ import {
   Post,
   Body,
   UploadedFile,
+  Delete,
+  Param,
   UseInterceptors,
   UsePipes,
   ValidationPipe,
@@ -17,6 +19,9 @@ import {
   ApiBearerAuth,
   ApiUnauthorizedResponse,
   ApiInternalServerErrorResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiConflictResponse,
 } from '@nestjs/swagger';
 import { ErrorFilter } from 'src/middleware/error.middleware';
 import { ProjectService } from './project.service';
@@ -53,5 +58,21 @@ export class ProjectController {
       file,
       req?.user?.id,
     );
+  }
+
+  @ApiOperation({ summary: 'Delete the project' })
+  @ApiBearerAuth('Token')
+  @ApiOkResponse({ description: 'Project has succesfully deleted' })
+  @ApiUnauthorizedResponse({
+    description: 'User does not have access token. User unauthorized.',
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
+  @ApiNotFoundResponse({ description: 'Project not found' })
+  @ApiConflictResponse({
+    description: 'Error when deleting the project',
+  })
+  @Delete('/:projectId')
+  deleteProject(@Param('projectId') projectId: string): Promise<any> {
+    return this.projectService.deleteProject(projectId);
   }
 }
--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -11,6 +11,7 @@ import { CreateProjectDto } from './dto/create-project.dto';
 import { User } from 'src/user/schema/user.schema';
 import { CloudinaryService } from 'src/cdn-cloudinary/cloudinary.service';
 import { File } from 'src/general-schemas/file.schema';
+import { Task } from 'src/task/schema/task.schema';
 
 @Injectable()
 export class ProjectService {
@@ -18,6 +19,7 @@ export class ProjectService {
     @InjectModel(User.name) private userModel: Model<User>,
     @InjectModel(Project.name) private projectModel: Model<Project>,
     @InjectModel(File.name) private fileModel: Model<File>,
+    @InjectModel(Task.name) private taskModel: Model<Task>,
     @InjectConnection() private readonly connection: mongoose.Connection,
     private readonly cloudinaryService: CloudinaryService,
   ) {}
@@ -82,6 +84,66 @@ export class ProjectService {
         await this.cloudinaryService.deleteImage(fileId);
       }
       throw new ConflictException('Error when creating new project');
+    } finally {
+      transactionSession.endSession();
+    }
+  }
+
+  async deleteProject(projectId: string): Promise<any> {
+    const transactionSession = await this.connection.startSession();
+    try {
+      transactionSession.startTransaction();
+      const filesToDelete = [];
+
+      const project = await this.projectModel
+        .findById(projectId)
+        .populate({
+          path: 'tasks',
+          model: 'Task',
+          populate: {
+            path: 'file',
+            model: 'File',
+          },
+        })
+        .populate('file')
+        .session(transactionSession);
+
+      if (project.file) {
+        filesToDelete.push(project.file.file_path);
+        await this.fileModel
+          .findByIdAndDelete(project.file.id)
+          .session(transactionSession);
+      }
+
+      if (project.tasks && project.tasks.length) {
+        for (const task of project.tasks) {
+          filesToDelete.push(task.file.file_path);
+          await Promise.all([
+            this.fileModel
+              .findByIdAndDelete(task.file.id)
+              .session(transactionSession),
+            this.taskModel
+              .findByIdAndDelete(task.id)
+              .session(transactionSession),
+          ]);
+        }
+      }
+
+      await this.projectModel
+        .findByIdAndDelete(projectId)
+        .session(transactionSession);
+
+      // Выношу отдельно так как если будет ошибка ДО, мы не будет удалять файлы с CDN , а если удалят файлы сразу то можем в дальнейших операциях потерять файлы
+      const deletePromises = filesToDelete.map((fileId) =>
+        this.cloudinaryService.deleteImage(fileId),
+      );
+      await Promise.all(deletePromises);
+
+      await transactionSession.commitTransaction();
+      return { message: 'Project has successfully deleted' };
+    } catch (err) {
+      await transactionSession.abortTransaction();
+      throw new ConflictException('Error when deleting the project');
     } finally {
       transactionSession.endSession();
     }
